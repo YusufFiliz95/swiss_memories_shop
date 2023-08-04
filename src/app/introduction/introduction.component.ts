1723c9370650ba269d6e79b32e34261a762df49e
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-08-04 16:39:43 +0200

    made a body animation like a sunset

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./introduction.component.scss']
 })
 export class IntroductionComponent {
+  stars: any[] = [];
 
+  constructor() {
+    this.createStars(100);
+  }
+
+  createStars(count: number) {
+    for (let i = 0; i < count; i++) {
+      this.stars.push({
+        id: i,
+        top: this.randomPosition(),
+        left: this.randomPosition()
+      });
+    }
+  }
+
+  randomPosition() {
+    return Math.floor(Math.random() * 100) + '%';
+  }
 }
